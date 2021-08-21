caff8408a79f468d1469461e249bb421e8b1834e
Rusl10 <cloud_47@mail.ru>
2021-08-21 07:47:33 +0300

    added calc c

---
+++ src/components/Calc.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="calc">
+    11
+  </div>
+</template>
+<script>
+export default {
+  name: 'Calc',
+  data () {
+    return {
+      type: 'usual'
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+</style>
